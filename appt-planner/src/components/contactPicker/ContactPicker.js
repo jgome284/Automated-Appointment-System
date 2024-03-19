9d83430bc15e4c037edaa15bf837d290c9fb09f1
Jose M. Gomez <gomezjosem@live.com>
2024-03-19 21:33:24 +0000

    ContactPicker Component with dropdown of memoized contacts

--- appt-planner/src/components/contactPicker/ContactPicker.js
+++ appt-planner/src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({value, name, handleChange, contacts}) => {
   return (
-    <></>
+    <select aria-label="Contact Picker" value={value} name={name} id="contact" onChange={handleChange}>
+      <option value={""} key={-1}>
+        No Contact Selected
+      </option>
+      {contacts.map((contact) => {
+        return (
+          <option value={contact} key={contact}>
+            {contact}
+          </option>
+        );
+      })}
+    </select>
   );
 };
